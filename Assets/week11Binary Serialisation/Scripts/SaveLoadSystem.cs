7fe4818cdac40282004245d4a30fb6d56aed5828
El-Parto <luc980@y7mail.com>
2021-05-05 16:08:26 +1000

    json amd binary

--- Assets/week11Binary Serialisation/Scripts/SaveLoadSystem.cs
+++ Assets/week11Binary Serialisation/Scripts/SaveLoadSystem.cs
@@ -7,15 +7,19 @@ namespace Serialisation
     public class SaveLoadSystem : MonoBehaviour
     {
         // * Streaming assets is a folder that Unity creates that we can use to
-        // * to load/save data in, in the Editor ut us ub tge orihect fikderm
-        // *  in a buld, it is in the exe's build folder.
+        // * to load/save data in, in the Editor it uses ub the orihect fikderm
+        // *  in a build, it is in the exe's build folder.
         private string FilePath => Application.streamingAssetsPath + "/gameData";
 
         [SerializeField] private bool useBinary = false;
 
         [SerializeField] private GameData gameData = new GameData();
 
-
+        private void Start()
+        {
+            if (!Directory.Exists(Application.streamingAssetsPath))
+                Directory.CreateDirectory(Application.streamingAssetsPath);
+        }
 
         void Save()
         {
@@ -33,6 +37,21 @@ namespace Serialisation
 
         private void SaveBinary()
         {
+
+            // * This opens the "river" between the RAM and the file.
+            // creating te river                         file loc'             river? Important.
+            using (FileStream stream = new FileStream(FilePath + ".save", FileMode.OpenOrCreate))
+            {
+                
+                // like creating the boat that will carry the data from one point to another.
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                //give memory stream
+                //* transports the data from the RAM to the specified file
+                // * like freezing water into ice.
+                formatter.Serialize(stream, gameData);
+
+            }
 
         }
 
@@ -62,33 +81,48 @@ namespace Serialisation
 
         private void LoadBinary()
         {
+            //if there is no save data we shouldn't attempt to load it
+            if (!File.Exists(FilePath + ".save"))
+                return;
 
+
+
+            // * This opens the "river" between the RAM and the file.
+            // creating te river                         file loc'             river? Important.
+            using (FileStream stream = new FileStream(FilePath + ".save", FileMode.Open))
+            {
+
+                // like creating the boat that will carry the data from one point to another.
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                gameData = formatter.Deserialize(stream) as GameData;
+
+            }
         }
+        
         private void LoadJson()
         {
-
+            // this is how we read the string data from a file
+            string json = File.ReadAllText(FilePath + ".json");
+            // * This is how you convert the json back to a data type.
+            // * The Generic is required for making sure the returned data is 
+            //* the same as the passed in
+            gameData = JsonUtility.FromJson<GameData>(json);
         }
-
-
-
 
         private void OnGUI()
         {
             if (GUILayout.Button("Save"))
             {
                 // Do that save jimmy!
+                Save();
             }
-
 
             if (GUILayout.Button("Load"))
             {
                 // Do that Load jimmy! Sure thing tommy!
+                Load();
             }
-
-
-            
-
-
         }
     }
 }
